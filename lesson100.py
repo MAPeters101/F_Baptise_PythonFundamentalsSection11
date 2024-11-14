f19f68789eb4dfcf747893d686eb5b228053853a
Mark Peters <mapeters101@earthlink.net>
2024-11-14 12:43:40 -0500

    Lesson100:  Work on lesson to 5:10

---
+++ lesson100.py
@@ -0,0 +1,17 @@
+
+# a = 1
+# b = 0
+# result = a / b
+# print(result)
+
+ex = ValueError('Name must be at least 5 characters long.')
+print(type(ex))
+print(ex)
+print(repr(ex))
+print(str(ex))
+
+#raise ex
+
+
+
+
