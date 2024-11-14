8b3b974548c02749e8ed57a66bf13cb5e8cfd32b
Mark Peters <mapeters101@earthlink.net>
2024-11-14 13:26:40 -0500

    Lesson102:  Work on lesson from 7:30 to 18:00.

--- lesson102.py
+++ lesson102.py
@@ -32,5 +32,93 @@ print('Code resumes here...')
 print()
 
 
+l = [1, 2, 3, 4, 5]
+try:
+    while True:
+        print(l.pop())
+except Exception:
+    print('Something unexpected happened.')
+print('Code resumes here...')
+print('='*80)
+
+
+l = (1, 2, 3, 4, 5)
+try:
+    while True:
+        print(l.pop())
+except Exception:
+    print('All done - all elements have been popped.')
+print('Code resumes here...')
+print(l)
+print()
+
+
+# l = (1, 2, 3, 4, 5)
+# try:
+#     while True:
+#         print(l.pop())
+# except IndexError:
+#     print('All done - all elements have been popped.')
+# print('Code resumes here...')
+# print(l)
+# print()
+
+print('='*80)
+#print(10 + 'abc')
+#print(10 + None)
+print(10)
+print()
+
+data = [10, 20, 10, 10, 5, 10]
+sum_data = 0
+count_data = 0
+for element in data:
+    sum_data += element
+    count_data += 1
+average = sum_data / count_data
+print(f'average = {average}')
+print()
+
+
+# data = []
+# sum_data = 0
+# count_data = 0
+# for element in data:
+#     sum_data += element
+#     count_data += 1
+# average = sum_data / count_data
+# print(f'average = {average}')
+# print()
+
+
+data = []
+if len(data) == 0:
+    average = 0
+else:
+    sum_data = 0
+    count_data = 0
+    for element in data:
+        sum_data += element
+        count_data += 1
+    average = sum_data / count_data
+print(f'average = {average}')
+print()
+
+
+data = [10, 20, 'abc']
+sum_data = 0
+count_data = 0
+try:
+    for element in data:
+        sum_data += element
+        count_data += 1
+    average = sum_data / count_data
+except ZeroDivisionError:
+    average = 0
+except TypeError:
+    average = 0
+print(f'average = {average}')
+print()
+
 
 
